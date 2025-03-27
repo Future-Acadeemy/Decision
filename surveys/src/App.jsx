1cbaa8e17c435e65f8b654fdcaa53d10a1293daa
Omar Khaled <72626558+omar-vtc@users.noreply.github.com>
2025-03-27 04:49:52 +0200

    commit

--- surveys/src/App.jsx
+++ surveys/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import Survey from "./components/Survey";
 import UserInfoForm from "./components/UserInfoForm";
 import "./index.css";
@@ -10,6 +12,8 @@ import SurveyForm from "./pages/SurveyForm";
 import Report from "./pages/Report";
 import DecisionMatrix from "./components/DecisionMatrix";
 import Home from "./components/Home";
+import Login from "./pages/Login";
+const queryClient = new QueryClient(); // Create QueryClient instance
 
 const App = () => {
   const [userInfo, setUserInfo] = useState(null);
@@ -25,16 +29,18 @@ const App = () => {
     // </Layout>
     // <Registeration />
     // <SurveyForm />
-
-    <Router>
-      <Routes>
-        <Route path="/" element={<Registeration />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/survey" element={<SurveyForm />} />
-        <Route path="/report" element={<Report />} />
-        <Route path="/decision" element={<DecisionMatrix />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/registeration" element={<Registeration />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/survey" element={<SurveyForm />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="/decision" element={<DecisionMatrix />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
 
     // <DecisionMatrix />
     // <Home />
